cc27f0b02d66e6e0dd4f1e6f63cacf03d133dcfa
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-11-15 18:05:55 -0500

    Update tic_tac_toe-v4.js

--- tic_tac_toe-v4.js
+++ tic_tac_toe-v4.js
@@ -148,7 +148,7 @@ function handleCellClick(event)
     const gameBoardIndex = parseInt(selectedCell.getAttribute('data-cell-index'));
 
     //checks if board isn't empty and not active
-    if (gameState[gameBoardIndex] != "" || !gameActive || mod[gameBoardIndex] == true)
+    if (gameState[gameBoardIndex] != "" || !gameActive || modState[gameBoardIndex] == true)
     {
         return;
     }
