1702ccc42ca0f8626989a75c90921692913a761f
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-11-15 15:24:44 -0500

    Update tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -27,6 +27,14 @@ function handleCellPlayed(selectedCell, gameBoardIndex)
     console.log("Game state location: " + gameState);
     console.log("Current player turn: " + currentPlayer);
     console.log("Index in array: " + gameBoardIndex);
+    var msg = new SpeechSynthesisUtterance();
+    var msgTxt = (currentPlayer + " selected cell " + selectedCell + ".");
+    msg.text = msgTxt;
+    speechSynthesis.speak(msg);
+    msgTxt = "";
+    msgTxt = ("It is now " + currentPlayer+"'s turn.");
+    msg.text = msgTxt;
+    speechSynthesis.speak(msg);
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
